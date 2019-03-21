7b5953e2cab2df0c41149ad412adce37baa7e8b6
Jared Duquette <jared@digitalduquette.com>
2019-03-21 16:27:25 -0400

    Update workbooks.sql

--- workbooks.sql
+++ workbooks.sql
@@ -1,6 +1,30 @@
+/*  
+
+Author: Jared Duquette 
+            @DigitalDuquette
+            GitHub.com/DigitalDuquette
+            DigitalDuquette.com 
+Consuming Platform: Tableau
+Created on: MM-DD-YY
+
+Purpose:
+    Workbook data from server. 
+
+    Each record in workbooks is one workbook on server.
+
+*****Change Log*****
+
+Last Update        Name             Change Summary
+MM-DD-YY           DUQ               summaryHere
+
+
+
+*/ 
+
+
 SELECT 
     wkbk.description AS WorkbookDetails_about, 
-    CAST(CASE 
+    (CASE 
         /* PURPOSE:
         Checks About section for more text than the template expected: 
 
@@ -17,9 +41,9 @@ SELECT
                 AND wkbk.description LIKE '%Functional Expert%' 
                 AND wkbk.description LIKE '%Technical Expert%' 
             )
-        ) THEN 1 
-        ELSE 0 
-    END AS bit) AS ContainsAboutSection,  
+        ) THEN 'true'
+        ELSE 'false'
+    END)::BOOLEAN AS "Contains About Section",  
     wkbk.id AS WorkbookID, 
     wkbk.name AS WorkbookName, 
     wkbk.repository_url AS WorkbookViewURL,
@@ -27,35 +51,20 @@ SELECT
             Use in servername.com/#/views/[WorkbookViewURL]
         */
     wkbk.created_at AT TIME ZONE 'EST' AS "Created (EST)",
+    wkbk.first_published_at AT TIME ZONE 'EST' AS "First Published (EST)", 
     wkbk.updated_at AT TIME ZONE 'EST' AS "Updated (EST)", 
-    wkbk.*
+    wkbk.view_count AS WorkbookViewCount,
+    
+    wkbk.lock_version, 
+    wkbk.revision, 
+    wkbk.document_version, 
+    -- TODO: add joins to lookup names
+    wkbk.owner_id, 
+    wkbk.project_id, 
+    prj.name AS "Project Name"
+    
+    prj.* 
+    -- wkbk.*
 FROM workbooks AS wkbk 
--- WHERE 
---     length(wkbk.description) > 59
---     AND 
---     (
---         wkbk.description LIKE '%Process Owner%'
---         AND wkbk.description LIKE '%Functional Expert%' 
---         AND wkbk.description LIKE '%Technical Expert%' 
---     )
---     /* PURPOSE:
---         Checks About section for more text than the template expected: 
-
---             *Process Owner*: 
---             *Functional Expert*: 
---             *Technical Expert*: 
---     */
-
-
--- and wkbk.name = 'Information Technology Monthly Review'
-
-
--- LIMIT 10 
-/* 
-Monthly overview for Information Technology department metrics.  
-
-*Process Owner*: Chad Brouwer
-*Functional Expert*: Chad Brouwer
-*Technical Expert*: Jared Duquette
-
-*/
+    /* Each record == one workbook on server */
+    INNER JOIN projects AS prj ON ( wkbk.project_id = prj.id )
