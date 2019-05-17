34eddd205621561a4d72872ef9a34a44e3457254
Jared Duquette <jared@digitalduquette.com>
2019-05-17 08:44:04 -0400

    Create groups.sql

---
+++ groups.sql
@@ -0,0 +1,11 @@
+
+/*
+    what groups is someone in
+*/ 
+select 
+    * 
+from group_users as gu 
+    inner join users_view as uv on ( gu.user_id = uv.id )
+    left outer join groups on ( gu.group_id = groups.id )
+where 
+    uv.name = 'Tsbeer'
