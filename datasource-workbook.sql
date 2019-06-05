c8357326d80dfe46b8373ed3e4fad5796052912a
Jared Duquette <jared@digitalduquette.com>
2019-06-05 13:12:07 -0400

    cleanup
    cleaned up

--- datasource-workbook.sql
+++ datasource-workbook.sql
@@ -1,30 +1,28 @@
 /*
 	
-	inside data_connections, not all listings are the data sources that are in a workbook, this also represents all of the records inside of published data sources.  
+	What data sources are used? 
+
+	Shows published and non-published data sources from Excel workbooks to database connections. 
 */
 
 SELECT
-	 
-	ds.is_certified AS "Certified Data Source", 
-	ds.name AS "Tableau Data Source Name", 
-	dc.dbname AS "Data Source Name", 
-	dc.server AS "Data Source Server", 
+	ds.is_certified AS "Certified Datasource", 
+	ds.name AS "Tableau Datasource Name", 
+	dc.dbname AS "Datasource Name", 
+	dc.server AS "Datasource Server", 
 	ds.db_class AS "Data Connection Type", 
-	dc.username, 
-	dc.owner_type,
+	dc.username AS "User Name", 
+	dc.owner_type AS "Workbook or Datasource",
 	CASE 
 		WHEN dc.owner_type = 'Datasource' THEN ds.name  
 		ELSE wkb.name 
-	END AS "Workbook or data source name"
-	
-    -- dc.*, 
-    -- ds.* 
+	END AS "Workbook or Datasource name"
 FROM data_connections AS dc 
-    inner join datasources AS ds ON ( dc.datasource_id = ds.id )
-    left outer join workbooks as wkb ON ( wkb.id = dc.owner_id )
+    INNER JOIN datasources AS ds ON ( dc.datasource_id = ds.id )
+    LEFT OUTER JOIN workbooks AS wkb ON ( wkb.id = dc.owner_id )
  
 ORDER BY 
-	ds.name asc 
+	ds.name ASC 
 
 	
 
